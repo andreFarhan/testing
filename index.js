f3d4b4ae6a63020074b515b366b8e7ef01da4410
Andre Farhan <58926131+andreFarhan@users.noreply.github.com>
2023-04-13 09:23:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
     <script>
         $('#tap-role-manajemen').addClass('side-menu--active');
         const limit = 10
